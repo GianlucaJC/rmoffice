84c84a7b14da48ff2f3a9f3e046e6fcc880d612c
GianlucaJC <morescogianluca@gmail.com>
2024-02-08 17:08:34 +0100

    fix nascondi altrove

--- resources/views/all_views/main.blade.php
+++ resources/views/all_views/main.blade.php
@@ -514,10 +514,12 @@
 									?>
 								
 									
+									<div class='class_view'>
 									@if ($tab->SINDACATO!="1")
 									<a href="javascript:void(0)"  onclick="insert_frt({{$tab->ID_anagr}})">
 										<button type="button" class="btn btn-success btn-sm mb-2">Iscrivi FRT</button>
 									</a>
+									</div>
 									@endif
 									
 									
