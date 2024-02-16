1c98e49407e0246b45e55804e54659c6bdf62d9c
GianlucaJC <morescogianluca@gmail.com>
2024-02-16 15:38:17 +0100

    dele nota

--- resources/views/all_views/main.blade.php
+++ resources/views/all_views/main.blade.php
@@ -1065,7 +1065,7 @@
 
 					$view.="</td>";
 					$view.="<td style='vertical-align: middle;'>";
-					if (count($note[$tab->ID_anagr])>1 && $note_dati['id_user']==$id_user) {
+					if (count($note[$tab->ID_anagr])>1 && ($note_dati['id_user']==$id_user || $id_user==1)) {
 						$id_nota=$note_dati['id_nota'];
 						$view.="<a href='javascript:void(0)' onclick='dele_nota($id_nota,".$tab->ID_anagr.")'>";
 							$view.="<i class='far fa-trash-alt'></i>";
